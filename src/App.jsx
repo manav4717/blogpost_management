817146fb47f1a6cbc337128b2859aa94e2376fcc
Manav Goswami <manavbharthigoswami29@gmail.com>
2026-02-12 13:42:36 +0530

    [MG][blogpost]

---
+++ src/App.jsx
@@ -0,0 +1,101 @@
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import "./App.css";
+import Dashboard from "./pages/Dashboard.jsx";
+import Login from "./Pages/Login.jsx";
+import Register from "./Pages/Register.jsx";
+import AuthGuard from "./auth/AuthGuard.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  const route = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <AuthGuard required={false}>
+          <DefaultRoute />
+        </AuthGuard>
+      ),
+    },
+    {
+      path: "/login",
+      element: (
+        <AuthGuard required={false}>
+          <Login />
+        </AuthGuard>
+      ),
+    },
+    {
+      path: "/register",
+      element: (
+        <AuthGuard required={false}>
+          <Register />
+        </AuthGuard>
+      ),
+    },
+    {
+      path: "/dashboard",
+      element: (
+        <AuthGuard required={true}>
+          <Dashboard />
+        </AuthGuard>
+      ),
+    },
+    {
+      path: "/create-post",
+      element: (
+        <AuthGuard required={true}>
+          <div
+            style={{ textAlign: "center", padding: "100px", color: "white" }}
+          >
+            <h1>Create Post Page</h1>
+            <p>This page is under construction.</p>
+          </div>
+        </AuthGuard>
+      ),
+    },
+    {
+      path: "*",
+      element: (
+        <div
+          style={{
+            textAlign: "center",
+            padding: "100px",
+            color: "white",
+          }}
+        >
+          <h1>404 - Page Not Found</h1>
+          <p>The page you are looking for does not exist.</p>
+        </div>
+      ),
+    },
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={route} />
+      <ToastContainer
+        position="top-right"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  );
+}
+
+const DefaultRoute = () => {
+  return <Navigate to="/dashboard" replace />;
+};
+
+export default App;
---
+++ src/pages/CreatePost.jsx
@@ -0,0 +1,98 @@
+import Navbar from "../components/Navbar"
+
+const CreatePost = () =>{
+    return(
+      <div className="create-post-page">
+        <Navbar/>
+        <div className="create-post-container">
+            <header className="form-header">
+                <h1>Create New Post</h1>
+                <p>Shere Your thoughts and stories with the world</p>
+            </header>
+            <div className="post-form-card">
+                <form>
+                    <div className="form-group">
+                        <lable>Post Title</lable>
+                        <div className="input-wrapper">
+                            <FaHeading className="input-icon"/>
+                            <input
+                                type="text"
+                                name="title"
+                                className="form-control"
+                                placeholder="Enter a catchy title....."
+                            />
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <label>Author Name</label>
+                        <div className="input-wrapper">
+                            <faUser className="input-icon"/>
+                            <input 
+                                type="text"
+                                name="author"
+                                className="form-control"
+                                placeholder="Your Name"
+                                />
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <label>Description</label>
+                        <textarea 
+                             name="drscription"
+                             className="form-control"
+                             placeholder="What's on your MIND ? write your story here"
+                             ></textarea>
+                    </div>
+                    <div className="form-group">
+                        <label>Cover Image</label>
+
+                        <div className="image-source-tabs">
+                            <button type="button" className="tab-btn active">
+                                Image URL
+                            </button>
+
+                            <button type="button" className="tab-btn">
+                                 Upload File
+                            </button>
+                        </div>
+
+                        <div className="input-wrapper">
+                            <FaLink className="input-icon"/>
+                            <input 
+                                 type="url"
+                                 name="imageUrl"
+                                 className="form-control"
+                                 placeholder="paste image URL here(eg . https://......)" 
+                                />
+                        </div>
+                        <div className="image-upload-area">
+                            <FaCloudUploadAlt className="upload-icon"/>
+                            <p>Click to Upload image from your device</p>
+                        </div>
+                        <div className="image-preview-container">
+                            <img src="https://avatars.mds.yandex.net/i?id=8213fb41d15052da6de6068415314b5fe2828ef2-7755581-images-thumbs&n=13"
+                                 alt="preview"
+                                 className="image-preview" 
+                                 />
+                                 <button type="button" className="remove-image-btn">
+                                    <FaTimes/>
+                                 </button>
+                        </div>
+                    </div>
+                    <div className="form-actions-row">
+                        <button type="submit" className="submit-btn">
+                            <FaRegPaperPlace /> Publish Post
+                        </button>
+
+                        <button type="button" className="cancel-btn">
+                            Clear Form
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+      </div>
+    )
+}
+
+export default CreatePost;
